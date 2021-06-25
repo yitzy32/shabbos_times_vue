ffbe4ceb3a3c885c701859ee54d15778fe20e2fb
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-24 21:13:22 -0400

    get all info for one day on page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+      <input type="text" v-model="search">
+      <button v-on:click="searchByZip()">Search</button> <br>
+      <h1>{{ selectedDay.date }}</h1>
+      <p>Location: {{ selectedDay.name }}</p>
+      <p>Mincha Gedola: {{ selectedDay.mincha_gedola }}</p>
+      <p>Early Mincha: {{ selectedDay.mincha }}</p>
+      <p>Plag HaMincha: {{ selectedDay.plag_mincha }}</p>
+      <p>Candel Lighting: {{ selectedDay.candle_lighting }}</p>
+      <p>Sunset: {{ selectedDay.sunset }}</p>
+      <p>Tzeis 50 MinUntes: {{ selectedDay.tzeis50min_fri_night }}</p>
+      <p>Tzeis 72 Minutes: {{ selectedDay.tzeis72min_fri_night }}</p>
   </div>
 </template>
 
@@ -8,13 +19,26 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      search: "",
+      selectedDay: [],
     };
   },
   created: function () {},
-  methods: {},
+  methods: {
+    searchByZip: function () {
+      var params = {
+        search: this.search,
+      };
+      axios.get(`/api/zmanims?search=${this.search}`).then((response) => {
+        console.log(response.data);
+        this.selectedDay = response.data;
+      });
+    },
+  },
 };
 </script>
