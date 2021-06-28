dc80ea37b4d0e5599a038a4bf7ec353cb601a30c
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-28 11:10:40 -0400

    dynamic table install and application

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Zmanim For This Friday</router-link> |
+      <router-link to="/calendar">Zmanim For The Entire Summer</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,48 +3,32 @@
     <h1>{{ message }}</h1>
     <input type="text" v-model="search">
     <button v-on:click="getWholeCalendar()">Search</button>
-    <div v-for="day in calendar" id="grid-container">
-      <div id="grid-item">
-        <h2>{{ day.date }}
-        {{ day.name }}<br></h2>
-        <b>Mincha Gedola </b>{{ day.mincha_gedola }}<br>
-        <b>Plag HaMincha</b>{{ day.plag_hamincha }}<br>
-        <b>Mincha: </b>{{ day.mincha }}<br>
-        <b>Candle Lighting: </b>{{ day.candle_lighting }}<br>
-        <b>Sunset: </b>{{ day.sunset }}<br>
-        <b>Tzeis 50 Minutes</b>{{ day.tzeis50min_fri_night }}<br>
-        <b>Tzeis 72 Minutes</b>{{ day.tzeis72min_fri_night }}<br>
-        <hr>
-      </div>
-    </div>
-    <!-- {{ calendar }} -->
+    <vue-table-dynamic :params="params"></vue-table-dynamic>
   </div>
 </template>
 
 <style>
-#grid-container {
-  display: grid;
-  /* grid-template-columns: 1fr 1fr 1fr; */
-  gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
-}
-
-/* #grid-item {
-  grid-column: 1 / 1;
-} */
 </style>
 
 <script>
+import VueTableDynamic from "vue-table-dynamic";
 import axios from "axios";
+
 export default {
-  data: function () {
+  name: "Demo",
+  data() {
     return {
-      message: "Whole calendar",
+      message: "Whole Calendar",
       search: "",
-      calendar: [],
+      params: {
+        data: [["Date", "Mincha Gedola", "Mincha", "Plag", "Candle Lighting", "Shkia", "Tzeis 50 Minutes"]],
+        header: "row",
+        border: true,
+        stripe: true,
+      },
     };
   },
-  created: function () {},
+  components: { VueTableDynamic },
   methods: {
     getWholeCalendar: function () {
       var params = {
@@ -52,7 +36,20 @@ export default {
       };
       axios.get(`/api/calendar?search=${this.search}`).then((response) => {
         console.log(response.data);
-        this.calendar = response.data;
+        response.data.forEach((day) => {
+          let nextRow = [];
+          nextRow.push(
+            day.date,
+            day.mincha_gedola,
+            day.mincha,
+            day.plag_hamincha,
+            day.candle_lighting,
+            day.sunset,
+            day.tzeis50min_fri_night
+          );
+          this.params.data.push(nextRow);
+        });
+        console.log(this.params.data);
       });
     },
   },
